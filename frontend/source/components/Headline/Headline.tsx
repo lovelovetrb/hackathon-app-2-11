85c3fe768035399ed9edd84603bc96c810129d25
mizuki <trombonelover8888@gmail.com>
2023-02-11 14:43:44 +0900

    refactor

--- frontend/source/components/Headline/Headline.tsx
+++ frontend/source/components/Headline/Headline.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Headline = ({ subject }: Props) => {
   return (
     <div className={css.div}>
-      <Image src="/peach.png" width={64} height={64} alt="My avatar" />
+      <Image src="/peach.png" width={64} height={64} alt="peach???" />
       <h1 className={css.h1}>{subject}</h1>
     </div>
   )
