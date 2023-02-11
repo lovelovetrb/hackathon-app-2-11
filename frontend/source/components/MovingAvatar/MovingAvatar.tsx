4554e3344911fbc502329d5b6394aad23b237dd1
mizuki <trombonelover8888@gmail.com>
2023-02-11 23:31:02 +0900

    many changes

--- frontend/source/components/MovingAvatar/MovingAvatar.tsx
+++ frontend/source/components/MovingAvatar/MovingAvatar.tsx
@@ -1,47 +1,51 @@
 import css from "components/MovingAvatar/MovingAvatar.module.css"
 import Image from 'next/image'
 import { data } from "types"
+import Statusbar from "components/Statusbar/Statusbar"
 
 type props = {
-    data: data[]
+  data: data[]
 }
 
 const MovingAvatar = ({ data }: props) => {
-    const i = data
-    return (
-        <div className={css.imageFlex}>
-            <Image
-                src="/NoMaskMan.png"
-                className={`${css.keyframe2} ${css.animation} ${css.image}`}
-                alt="My avatar"
-                fill
-            />
-            <Image
-                src="/MaskBuisinesMan.png"
-                className={`${css.keyframe2} ${css.animation} ${css.image}`}
-                alt="My avatar"
-                fill
-            />
-            <Image
-                src="/MaskBuisinesMan.png"
-                className={`${css.keyframe2} ${css.animation} ${css.image}`}
-                alt="My avatar"
-                fill
-            />
-            <Image
-                src="/MaskBuisinesMan.png"
-                className={`${css.keyframe2} ${css.animation} ${css.image}`}
-                alt="My avatar"
-                fill
-            />
-            <Image
-                src="/MaskBuisinesMan.png"
-                className={`${css.keyframe2} ${css.animation} ${css.image}`}
-                alt="My avatar"
-                fill
-            />
-        </div>
-    )
+  const i = data
+  return (
+    <>
+      <div className={css.imageFlex}>
+        <Image
+          src="/NoMaskMan.png"
+          className={`${css.keyframe2} ${css.animation} ${css.image}`}
+          alt="My avatar"
+          fill
+        />
+        <Image
+          src="/MaskBuisinesMan.png"
+          className={`${css.keyframe2} ${css.animation} ${css.image}`}
+          alt="My avatar"
+          fill
+        />
+        <Image
+          src="/MaskBuisinesMan.png"
+          className={`${css.keyframe2} ${css.animation} ${css.image}`}
+          alt="My avatar"
+          fill
+        />
+        <Image
+          src="/MaskBuisinesMan.png"
+          className={`${css.keyframe2} ${css.animation} ${css.image}`}
+          alt="My avatar"
+          fill
+        />
+        <Image
+          src="/MaskBuisinesMan.png"
+          className={`${css.keyframe2} ${css.animation} ${css.image}`}
+          alt="My avatar"
+          fill
+        />
+      </div>
+      <Statusbar />
+    </>
+  )
 }
 
 
