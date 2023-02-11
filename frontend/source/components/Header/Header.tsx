82e85677683407db0f6f2828032f4e8e76103cb2
mizuki <trombonelover8888@gmail.com>
2023-02-12 03:36:28 +0900

    create axios routing

--- frontend/source/components/Header/Header.tsx
+++ frontend/source/components/Header/Header.tsx
@@ -1,7 +1,23 @@
-const Header = () => {
-  return(
-  <>aa</>
-  )
-  }
+import { Shippori_Antique, Yuji_Mai } from "@next/font/google"
+import css from "components/Header/Header.module.css"
 
-  export default Header
+
+const font_header = Shippori_Antique({
+  weight: "400",
+  subsets: ["latin"]
+})
+
+
+const Header = () => {
+  return (
+    <>
+      <header className={`${css.header} ${font_header.className}`}>
+        <h3 className={css.title}>
+          マスク電鉄
+        </h3>
+      </header>
+    </>
+  )
+}
+
+export default Header
